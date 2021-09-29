9bc935778a70f72944dd9a1e8c3ad15a1b070865
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-09-29 16:48:01 +0100

    Default arguments

---
+++ 1.Functions/DefaultArgumentDemo.py
@@ -0,0 +1,11 @@
+from typing import DefaultDict
+
+
+def printArea(width = 1, height = 2):
+    area = width * height
+    print("width:", width, "\theight:", height, "\tarea:", area)
+
+printArea() # Default  arguments
+printArea(4, 2.5)
+printArea(width = 1.2)
+printArea(height = 6.2)
