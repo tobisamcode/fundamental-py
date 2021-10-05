4a41781ca4b09658137f45b30295d4a5e8b5fd00
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-10-05 17:14:20 +0100

    ...

--- 2.Objects_And_Classes/TestPassMutableObjects.py
+++ 2.Objects_And_Classes/TestPassMutableObjects.py
@@ -21,3 +21,28 @@ class Circle:
 
     def getArea(self):
         return self.radius * self.radius * math.pi
+
+
+# print a tables of areas for radius
+def printAreas(c, times):
+        print("Radius \t\tAreas")
+        while times >= 1:
+            print(c.radius, "\t\t", c.getArea())
+            c.radius = c.radius + 1
+            times = times - 1 
+
+
+def main():
+    # Create a circle object with radius 1
+    myCircle = Circle()
+
+    # Print areas for radius 1, 2, 3, 4, and 5
+    n = 5
+    printAreas(myCircle, n)
+
+    # Display myCircle.radius and times
+
+    print("\nRadius is", myCircle.radius)
+    print("n is", n)
+
+main()
