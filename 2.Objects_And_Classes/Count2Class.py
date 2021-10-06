69835a38298d1fe41c5a7df43d9fae349bc9c98a
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-10-06 18:23:23 +0100

    changes added

---
+++ 2.Objects_And_Classes/Count2Class.py
@@ -0,0 +1,25 @@
+'''Show the output of the following program:'''
+
+class Count:
+    def __init__(self, count = 0):
+        self.count = count
+
+def main():
+        c = Count()
+        n = 1
+        m(c, n)
+
+        print('count is', c.count)
+        print('n is', n)
+
+
+def m(c, n):
+    c = Count(5)
+    n = 3
+
+main()
+
+'''Output
+count is 0
+n is 1
+'''
