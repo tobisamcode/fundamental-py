1ff7a0fd402a15f2c23b711bf50faf1965c535d5
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-10-10 18:58:37 +0100

    changes added

---
+++ 2.Objects_And_Classes/UseBMIClass.py
@@ -0,0 +1,47 @@
+class BMI:
+    def __init__ (self, name, age, weight, height):
+        self.__name = name
+        self.__age = age
+        self.__weight = weight
+        self.__height = height
+
+    def getBMI(self):
+        KILOGRAMS_PER_POUND = 0.45359237
+        METERS_PER_INCH = 0.0254
+
+        bmi = self.__weight * KILOGRAMS_PER_POUND / ((self.__height * METERS_PER_INCH) ** 2)
+        return round(bmi * 100) / 100
+
+    def getStatus(self):
+        bmi = self.getBMI
+        if bmi < 18.5:
+            return "Underweight"
+        elif bmi < 25:
+            return "Normal"
+        elif bmi < 30:
+            return "Overweight"
+        else:
+            return "Obese"
+        
+    def getName(self):
+        return self.__name
+
+    def getAge(self):
+        return self.__age
+
+    def getWeight(self):
+        return self.__weight
+
+    def getHeight(self):
+        return self.__height
+
+
+def main():
+    bmi1 = BMI("John Doe", 18, 145, 70)
+    print("The BMI for", bmi1.getName(), "is", bmi1.getBMI(), bmi1.getStatus())
+
+    bmi2 = BMI("Peter KIng", 50, 215, 70)
+    print("The BMI for", bmi2.getName(), "is", bmi2.getBMI(), bmi2.getStatus())
+
+
+main()
