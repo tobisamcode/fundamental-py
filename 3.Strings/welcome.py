4d33085b38d9df3782f18fdacdd6db716304469b
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-10-18 18:16:24 +0100

    string

---
+++ 3.Strings/welcome.py
@@ -0,0 +1,11 @@
+# Creating Strings
+'''You can create strings by using the constructor, as follows:'''
+s1 = str() # Craet an empty string object
+s2 = str('Welcome') # Create a string object for welcome
+
+
+# Functions for Strings
+s = "welcome"
+print(len(s)) # 7
+print(max(s)) # o
+print(min(s)) # W
